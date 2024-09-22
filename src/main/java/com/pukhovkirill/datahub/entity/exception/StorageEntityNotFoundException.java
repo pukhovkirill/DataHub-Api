1bb90843b36e9de3af051de813e083880fdcedae
Pukhov Kirill <kirillpuhov526@gmail.com>
2024-09-22 22:11:17 +0300

    feat: add basic domain entity

---
+++ src/main/java/com/pukhovkirill/datahub/entity/exception/StorageEntityNotFoundException.java
@@ -0,0 +1,5 @@
+package com.pukhovkirill.datahub.entity.exception;
+
+public class StorageEntityNotFoundException extends Exception{
+
+}
---
+++ src/main/java/com/pukhovkirill/datahub/entity/gateway/StorageGateway.java
@@ -0,0 +1,5 @@
+package com.pukhovkirill.datahub.entity.gateway;
+
+public interface StorageGateway {
+
+}
