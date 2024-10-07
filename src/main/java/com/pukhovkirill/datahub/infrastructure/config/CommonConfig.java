b06cad4d4e1e17070cf016d5d45574a79ea15695
Pukhov Kirill <kirillpuhov526@gmail.com>
2024-10-07 08:49:27 +0300

    feat: add config for OngoingGatewayService

--- src/main/java/com/pukhovkirill/datahub/infrastructure/config/CommonConfig.java
+++ src/main/java/com/pukhovkirill/datahub/infrastructure/config/CommonConfig.java
@@ -1,8 +1,20 @@
 package com.pukhovkirill.datahub.infrastructure.config;
 
+import com.pukhovkirill.datahub.entity.gateway.StorageGateway;
+import com.pukhovkirill.datahub.infrastructure.file.service.OngoingGatewayService;
+import com.pukhovkirill.datahub.infrastructure.file.service.OngoingGatewayServiceImpl;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class CommonConfig {
+
+    @Bean
+    @Scope("singleton")
+    public OngoingGatewayService ongoingGatewayService(@Qualifier("minioStorageGateway") StorageGateway gateway) {
+        return new OngoingGatewayServiceImpl(gateway);
+    }
 
 }
---
+++ src/test/java/com/pukhovkirill/datahub/infrastructure/file/service/CacheableStorageServiceImplTest.java
@@ -0,0 +1,5 @@
+package com.pukhovkirill.datahub.infrastructure.file.service;
+
+public class CacheableStorageServiceImplTest {
+
+}
