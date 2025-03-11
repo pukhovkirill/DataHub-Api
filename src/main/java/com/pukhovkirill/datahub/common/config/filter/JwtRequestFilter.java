01545212e55abc7186c25c3ba9ca716ddcf2ee63
Pukhov Kirill <kirillpuhov526@gmail.com>
2025-03-11 19:22:03 +0300

    chore: clean configurations

--- src/main/java/com/pukhovkirill/datahub/common/config/filter/JwtRequestFilter.java
+++ src/main/java/com/pukhovkirill/datahub/common/config/filter/JwtRequestFilter.java
@@ -16,7 +16,6 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
